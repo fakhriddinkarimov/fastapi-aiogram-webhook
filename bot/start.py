e9b04bcf420d1f21cff745bb6081ccfe27050974
fakhriddinkarimov <fakhriddindeveloper@gmail.com>
2022-08-30 23:11:27 +0500

    fix: webhook path

--- bot/start.py
+++ bot/start.py
@@ -1,8 +1,7 @@
-from aiogram import Dispatcher,Bot, types
-Token = ""
-bot = Bot(token=Token)
+from aiogram import Dispatcher, Bot, types
+bot = Bot(token="")
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    await message.answer(f"Hi, {message.from_user.full_name}")
+    await message.answer("Hi")
--- main.py
+++ main.py
@@ -1,25 +1,36 @@
+from aiogram.dispatcher.dispatcher import Dispatcher
 from fastapi import FastAPI
-from aiogram import types
-from bot.start import dp, bot,Token
+from aiogram import types, Dispatcher, Bot
+from bot.start import dp, bot
 
 app = FastAPI()
 
-webhook_path = f"/bot/{Token}"
-webhook_url = ""
+webhook_path = f"/bot/{bot}"
+webhook_url = "" + webhook_path
 
 
 @app.on_event("startup")
-async def on_bot():
+async def on_startup():
     webhook_info = await bot.get_webhook_info()
     if webhook_info.url != webhook_url:
-        await bot.set_webhook(url=webhook_url)
+        await bot.set_webhook(
+            url=webhook_url
+        )
+
 
 @app.post(webhook_path)
 async def bot_webhook(update: dict):
-    updater = types.Update(**update)
-    await dp.process_update(updater)
+    telegram_update = types.Update(**update)
+    Dispatcher.set_current(dp)
+    Bot.set_current(bot)
+    await dp.process_update(telegram_update)
 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 @app.on_event("shutdown")
 async def on_shutdown():
-    await bot.session.close()
+    pass
+
